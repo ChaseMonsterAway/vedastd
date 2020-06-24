5843ba0443d3f88e13c9a9b76cf2a92c070e873a
YuxinZou <zouyux@outlook.com>
2020-06-24 18:32:28 +0800

    update

--- vedastd/criteria/util.py
+++ vedastd/criteria/util.py
@@ -3,28 +3,25 @@ import numpy as np
 
 
 def ohem_single(pred_text, gt_text, mask):
-    pos_num = np.sum(gt_text > 0.5) - np.sum((gt_text > 0.5) & (mask <= 0.5))
+    pos_num = torch.sum(gt_text > 0.5) - torch.sum((gt_text > 0.5) & (mask <= 0.5))
 
     if pos_num == 0:
-        selected_mask = training_mask
-        selected_mask = selected_mask.reshape(1, selected_mask.shape[0], selected_mask.shape[1]).astype('float32')
-        return selected_mask
+        selected_mask = mask
+        return torch.unsqueeze(selected_mask, dim=0).float()
 
-    neg_num = np.sum(gt_text <= 0.5)
-    neg_num = (int)(min(pos_num * 3, neg_num))
+    neg_num = torch.sum(gt_text <= 0.5)
+    neg_num = min(pos_num * 3, neg_num)
 
     if neg_num == 0:
-        selected_mask = training_mask
-        selected_mask = selected_mask.reshape(1, selected_mask.shape[0], selected_mask.shape[1]).astype('float32')
-        return selected_mask
+        selected_mask = mask
+        return torch.unsqueeze(selected_mask, dim=0).float()
 
-    neg_score = score[gt_text <= 0.5]
+    neg_score = pred_text[gt_text <= 0.5]
     neg_score_sorted = np.sort(-neg_score)
     threshold = -neg_score_sorted[neg_num - 1]
 
-    selected_mask = ((score >= threshold) | (gt_text > 0.5)) & (training_mask > 0.5)
-    selected_mask = selected_mask.reshape(1, selected_mask.shape[0], selected_mask.shape[1]).astype('float32')
-    return selected_mask
+    selected_mask = ((pred_text >= threshold) | (gt_text > 0.5)) & (mask > 0.5)
+    return torch.unsqueeze(selected_mask, dim=0).float()
 
 
 def ohem_single_bk(score, gt_text, training_mask):
@@ -69,8 +66,13 @@ def ohem_batch(scores, gt_texts, training_masks):
 
 
 if __name__ == '__main__':
-    np.random.seed(0)
-    score = np.random.rand(224, 224)
-    gt_text = np.random.rand(224, 224)
-    training_mask = np.random.rand(224, 224)
-    print(ohem_single(score, gt_text, training_mask))
+    torch.manual_seed(1)
+    score = torch.rand(224, 224)
+    gt_text = torch.rand(224, 224)
+    training_mask = torch.rand(224, 224)
+    print(torch.sum(ohem_single(score, gt_text, training_mask)))
+
+    score_np = score.numpy()
+    gt_text_np = gt_text.numpy()
+    training_mask_np = training_mask.numpy()
+    print(np.sum(ohem_single_bk(score_np,gt_text_np,training_mask_np)))
